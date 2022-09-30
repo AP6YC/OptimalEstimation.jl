3110f0a932dd5b7de4c22fabb48c2c48de6c02ac
Sasha Petrenko <sap625@umsystem.edu>
2022-09-30 12:41:27 -0500

    Def modules, add figure

---
+++ src/common.jl
@@ -0,0 +1,16 @@
+"""
+    common.jl
+
+A collection of common definitions and utilities for the OptimalEstimation.jl package.
+"""
+
+# --------------------------------------------------------------------------- #
+# ABSTRACT TYPES
+# --------------------------------------------------------------------------- #
+
+"""
+    Filter
+
+An abstract type for all filters.
+"""
+abstract type Filter end
---
+++ src/modules/modules.jl
@@ -0,0 +1,9 @@
+"""
+    modules.jl
+
+This file aggregates all modules in the OptimalEstimation.jl package.
+"""
+
+include("KF.jl")    # Kalman Filter
+include("EKF.jl")   # Extended Kalman Filter
+include("UKF.jl")   # Unscented Kalman Filter
