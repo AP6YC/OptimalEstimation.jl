051a784289b78cad0f01ada4185b96524016c1c2
Sasha Petrenko <sap625@umsystem.edu>
2022-10-06 16:44:40 -0500

    Add imports line to module docstring

--- src/OptimalEstimation.jl
+++ src/OptimalEstimation.jl
@@ -3,10 +3,15 @@ Main module for `OptimalEstimation.jl`, a Julia package of optimal state estimat
 
 This module exports all of the modules, filters, and utilities used by the `OptimalEstimation.jl package.`
 
+# Imports
+
+The following names are imported to the package for internal use as dependencies:
+$(IMPORTS)
+
 # Exports
 
+The following names are available when `using OptimalEstimation`:
 $(EXPORTS)
-
 """
 module OptimalEstimation
 
